ef3328b32e845e54365531fab5cd6a4dc3f81de5
mahjabin <mahjabinvnsc@gmail.com>
2024-09-16 20:28:49 +0600

    loop

---
+++ loops.py
@@ -0,0 +1,12 @@
+#name="Helloworld"
+#for i in name:
+    #print(i)
+#for x in range(4,20):
+    #print(x)  
+
+inp=int(input("Enter your number"))
+sum = 0
+for i in range(1,inp+1):
+    sum+=i
+    print("sum = ", sum)
+print( "toatal sum" , sum)    
