a5be2ee230f37b750fff85d4f557ee252737b51c
mahjabin <mahjabinvnsc@gmail.com>
2024-09-16 12:08:01 +0600

    commit message

--- test03.py
+++ test03.py
@@ -1,6 +1,6 @@
 inp=int(input("Enter a number"))
 newnum=inp%10
 if newnum%3==0:
-    print("The last digit of the number is divisable by 322")
+    print("The last digit of the number is divisable by 3")
 else:
     print("The last digit of the number isn't divisable by 32")
