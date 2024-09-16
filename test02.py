640fc02bbd2229a0384afbf8d811462b3f647d56
mahjabin <mahjabinvnsc@gmail.com>
2024-09-16 12:05:50 +0600

    commit message

--- test02.py
+++ test02.py
@@ -3,4 +3,4 @@ newnum=inp%10
 if newnum%3==0:
     print("The last digit of the number is divisable by 3")
 else:
-    print("The last digit of the number isn't divisable by 3")
+    print("The last digit of the number isn't divisable by 32")
