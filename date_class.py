f68f4be3a6dbf701d10149b7d5260198b5e6d6d8
mahjabin <mahjabinvnsc@gmail.com>
2024-10-29 19:18:10 +0600

    jg

---
+++ date_class.py
@@ -0,0 +1,6 @@
+from datetime import date, time ,datetime
+today = date.today()
+now= datetime.now()
+print("Today's date is ",today)
+print(" current time:",now)
+print( "Day/Month/Year:",today.day, today.month ,today.year)
--- random_game.py
+++ random_game.py
@@ -1,8 +1,8 @@
 import random
 play=True 
-number=str(random.randint(1,50))
+number=str(random.randint(1,10))
 while play:
-    inp=input("Guess a number\n")
+    inp=input("Guess a number from 1 to 10\n")
     if inp==number:
         print("Congrats!Your guess is correct")
         print("The number was",number)
