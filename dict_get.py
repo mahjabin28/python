4b0577dbe28b23911c98d17552dd54b40d1b1a9c
mahjabin <mahjabinvnsc@gmail.com>
2024-11-05 19:52:03 +0600

    kjh

---
+++ dict_get.py
@@ -0,0 +1,9 @@
+country={'bangladesh':'088','nepal':'987','bhutan':'098'}
+print('bangladesh')
+print(country.get('bangladesh','not found'))
+print('Japan')
+print(country.get('Japan','not found'))
+         
+        
+         
+
