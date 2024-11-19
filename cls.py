cbb505a45d231f97a4e6e06ec98e36e86454943b
mahjabin <mahjabinvnsc@gmail.com>
2024-11-19 20:03:45 +0600

    jjb

---
+++ cls.py
@@ -0,0 +1,8 @@
+class Student:
+    name="Rita"
+    grade="7"
+    age="14"
+    print("sudent info:" , name,grade,age)
+s1=Student()
+print(s1.age)    
+print(s1.grade)
---
+++ obj.py
@@ -0,0 +1,16 @@
+class Parrot:
+    color="green"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+parrot1=Parrot("blu",3)
+parrot2=Parrot("blue",2)
+parrot3=Parrot("gr",2)
+parrot4=Parrot("pink",5)
+parrot5=Parrot("x",6)
+print(parrot1.age)
+print(parrot3.color)
+
+
+    
+    
