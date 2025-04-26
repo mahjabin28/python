4592fad523406406e636e9f60aba91b1e5ea8af5
mahjabin <mahjabinvnsc@gmail.com>
2025-04-26 16:30:15 +0600

    chapter1-3half

---
+++ class1.py
@@ -0,0 +1,19 @@
+print("Hello world")
+text="Miya Miya"
+print(text)
+number1=10
+number1=20
+
+number2=50
+result=number1+number2
+print(result)
+print(type(number1))
+v="308987"
+print(type(v))
+s=45.087
+print(type(s))
+s=""
+print(s)
+r=7+0.8
+print(r)
+print(4+2.8)
